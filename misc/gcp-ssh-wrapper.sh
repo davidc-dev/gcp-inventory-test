d372871884e8e8a6301e53c472e547c0f1e59888
David Clark <58272339+davidc-dev@users.noreply.github.com>
2025-04-17 11:04:54 -0500

    Update gcp-ssh-wrapper.sh

--- misc/gcp-ssh-wrapper.sh
+++ misc/gcp-ssh-wrapper.sh
@@ -18,4 +18,4 @@ for ssh_arg in "${@: 1: $# -3}" ; do
         fi
 done
 
-exec google-cloud-sdk/bin/gcloud compute ssh $opts "${host}" -- -C "${cmd}"
+exec google-cloud-sdk/bin/gcloud compute ssh $opts "{{ ssh_user }}@${host}" -- -C "${cmd}"
